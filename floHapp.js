057d3ce5d203f849cf4c97d8f41890cd858adc20
Florian Maurer <floh@TanteBerta.home>
2019-01-01 08:09:32 +0100

    First REST API implementation

--- floHapp.js
+++ floHapp.js
@@ -1,7 +1,61 @@
-const express = require('express')
-const app = express()
-const port = 3000
+//Use express framework
+var express = require('express');
+var app = express();
 
-app.get('/', (req, res) => res.send('Hello adf with floHapp once again'))
+//use fs 
+var fs = require("fs");
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+//Use public for static files
+app.use(express.static('public'));
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+   console.log("Got a POST request for the homepage");
+   res.send('Hello POST');
+})
+
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+   console.log("Got a DELETE request for /del_user");
+   res.send('Hello DELETE');
+})
+
+// This responds a GET request for the /list_user page.
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+    });
+ })
+
+ app.get('/getUser/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+
+
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function(req, res) {   
+   console.log("Got a GET request for /ab*cd");
+   res.send('Page Pattern Match');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
